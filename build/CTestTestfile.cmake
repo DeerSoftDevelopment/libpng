d58b4d060284afb51487e55627d03dfab3f23ee6
Mark-Andre Schadow <markandre@hotmail.de>
2023-01-19 09:17:22 +0100

    cleanup

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mark/Dokumente/Arbeit/libharu/deps/libpng
-# Build directory: /home/mark/Dokumente/Arbeit/libharu/deps/libpng/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
